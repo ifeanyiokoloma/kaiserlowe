c45616d5bce8fd8ec0de613646dcd36a8b1482f8
Ifeanyi Okoloma <ifeanyiokoloma@gmail.com>
2023-01-31 21:51:13 +0100

    Translated the site

--- src/component/LearnMore.js
+++ src/component/LearnMore.js
@@ -1,13 +1,15 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
 
 const LearnMore = ({color, bgc}) => {
+  const {t} = useTranslation()
   return (
     <div className="text-center mt-5">
       <button
         style={{ color: color, background: bgc }}
         className="btn btn-light poppins"
       >
-        Learn more
+        {t("Learn more")}
       </button>
     </div>
   );
--- src/component/Training.js
+++ src/component/Training.js
@@ -3,12 +3,14 @@ import { training } from "../asset/data";
 import { Container } from "@mui/material";
 import LearnMore from "./LearnMore";
 import { StyledTraining } from "../styles/StyledTraining";
+import { useTranslation } from "react-i18next";
 
 const Training = () => {
+  const {t} = useTranslation()
   return (
     <StyledTraining className="pb-5">
       <Container>
-        <h1 className="text-center text-uppercase">Training</h1>
+        <h1 className="text-center text-uppercase">{t("Training")}</h1>
         <div className="cards">
           {training.map((item) => (
             <div className="card">
@@ -16,10 +18,10 @@ const Training = () => {
                 <img src={item.img} alt="" />
               </div>
               <section className="pt-4 text-light card-text">
-                <h2 className="ps-3 text-uppercase">{item.h2}</h2>
+                <h2 className="ps-3 text-uppercase">{t(item.h2)}</h2>
                 <ul>
                   {item.list.map((item) => (
-                    <li>{item}</li>
+                    <li>{t(item)}</li>
                   ))}
                 </ul>
               </section>
