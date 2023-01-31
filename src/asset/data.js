535dfefda0e9556674f48ab388df0ee141f6dc30
Ifeanyi Okoloma <ifeanyiokoloma@gmail.com>
2023-01-31 13:01:56 +0100

    Modified data.js

--- src/asset/data.js
+++ src/asset/data.js
@@ -1,1 +1,40 @@
-export const navItems = ["Home", "About", "Contact", "Services", "Trainning", "Portfolio", "Team"];
+export const navItems = [
+  "Home",
+  "About",
+  "Services",
+  "Trainning",
+  "Portfolio",
+  "Team",
+  "Contact",
+];
+
+export const heroText = [
+  {
+    h1: "Welcome to KaiserLowe",
+    p: "Welcome to Kaiser Lowe, where construction excellence meets unparalleled customer service.",
+    img: "/images/hero/complex-road.jpg",
+  },
+  {
+    h1: "Building the Future",
+    p: "Kaiser Lowe Construction is a leading construction company dedicated to providing top-notch building solutions to clients.",
+    img: "/images/hero/construction-worker.jpg",
+  },
+  {
+    h1: "Dreams come true",
+    p: "We are dedicated to turning your dreams into reality with our exceptional construction services and unwavering commitment to quality.",
+    img: "/images/hero/two-construction-workers.jpg",
+  },
+  {
+    h1: "Experience the Difference",
+    p: "Discover the difference that comes with partnering with a construction company that truly cares. At Kaiser Lowe, we are dedicated to delivering projects that exceed expectations.",
+    img: "/images/hero/complex-road.jpg",
+  },
+];
+
+export const about = [
+  {
+    h2: "About us",
+    content:
+      "Kaiser Löwe starts 2013 to operates in Nigeria as a building development company. We offer the whole spectrum of services in building and managing. If you are interested for more information just take a look an enjoy… Our business philosophy is based on continuity and organic growth, on team spirit, respect and partnership. For us, high tech, tradition and diligence go hand in hand with a sense for the more pleasant things in life.",
+  },
+];
