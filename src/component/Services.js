58df7dcbdf8d109bc1531601809287d0519cf47c
Ifeanyi Okoloma <ifeanyiokoloma@gmail.com>
2023-02-06 02:15:02 +0100

    Added translation to services

--- src/component/Services.js
+++ src/component/Services.js
@@ -2,21 +2,23 @@ import React from "react";
 import StyledServices from "../styles/StyledServices";
 import { Box, Container, Typography } from "@mui/material";
 import { services } from "../asset/content";
+import { useTranslation } from "react-i18next";
 
 const Services = () => {
+  const {t} = useTranslation()
   return (
     <Container>
       <StyledServices className="my-5">
         {services.map((service) => (
           <div key={service.h2} className="myCard d-flex flex-column">
             <article>
-              <h2>{service.h2}</h2>
+              <h2>{t(service.h2)}</h2>
               <Typography
                 component="p"
                 variant="body2"
                 className="text-justify"
               >
-                {service.p}
+                {t(service.p)}
               </Typography>
             </article>
             <Box>
